41ebbdf226e09e9eef479451337b1c9776fd54af
KEERTHI <keerthimanku7@gmail.com>
2023-11-21 11:26:21 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+def BTS(request):
+    return render(request,'BTS.html')
+def jin(request):
+    return render(request,'jin.html')
+def jungkook(request):
+    return render(request,'jungkook.html')
